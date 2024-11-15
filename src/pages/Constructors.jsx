ca83e37b675cbfed475e24b26f5893d91e7de167
Karthik-Parvathaneni <karthikparvathaneni2@gmail.com>
2024-11-15 10:17:33 +0530

    Update Constructors.jsx

--- src/pages/Constructors.jsx
+++ src/pages/Constructors.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import ConstructorModal from "../components/COnstructorModal";
+import ConstructorModal from "../components/ConstructorModal";
 import RaceStatsModal from "../components/RaceStatsModal";
 
 const Constructors = () => {
